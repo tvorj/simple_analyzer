b774f3c259080830a8c2dcfaf42847c6bbe78645
root <root@LAPTOP-9VLDA4MN.localdomain>
2025-11-05 21:29:44 +0400

    added average and range_diff

--- simple_analyzer/analyzer.py
+++ simple_analyzer/analyzer.py
@@ -30,3 +30,8 @@ class Analyzer:
                 count += 1
         return count
 
+    def average(self):
+        return (sum(self.numbers) / len(self.numbers)) if self.numbers else None
+
+    def range_diff(self):
+        return (max(self.numbers) - min(self.numbers)) if self.numbers else None
