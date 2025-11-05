4991261e6f3e62352859c7ab53f8496864748489
root <root@LAPTOP-9VLDA4MN.localdomain>
2025-11-05 20:44:32 +0400

    content in analyzer.py

---
+++ simple_analyzer/analyzer.py
@@ -0,0 +1,32 @@
+class Analyzer:
+
+    def __init__(self):
+        self.numbers = []
+
+    def add_number(self, x: int):
+        self.numbers.append(x)
+
+    def even_count(self) -> int:
+        count = 0
+        for num in self.numbers:
+            if num % 2 == 0:
+                count += 1
+        return count
+
+    def odd_count(self) -> int:
+        count = 0
+        for num in self.numbers:
+            if num % 2 == 1:
+                count += 1
+        return count
+
+    def highest_number(self):
+        return max(self.numbers) if self.numbers else None
+
+    def increasing_pairs(self) -> int:
+        count = 0
+        for i in range(1, len(self.numbers)):
+            if self.numbers[i] > self.numbers[i - 1]:
+                count += 1
+        return count
+
