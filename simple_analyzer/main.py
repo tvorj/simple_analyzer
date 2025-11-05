4058ad1bb3bd3371a81c32b27d130169322e0ea7
root <root@LAPTOP-9VLDA4MN.localdomain>
2025-11-05 20:55:10 +0400

    added main logic in main.py

--- simple_analyzer/main.py
+++ simple_analyzer/main.py
@@ -1,3 +1,8 @@
+from simple_analyzer.analyzer import Analyzer
+import random
+import time
+from datetime import datetime
+
 def read_interval():
     with open('config/config.txt', 'r') as f:
         values = {}
@@ -12,8 +17,32 @@ def read_interval():
     return values['interval'], values['sequence_length']
 
 
-if __name__ == "__main__":
+def main():
     interval, sequence_length = read_interval()
 
     print(f'interval : {interval}')
     print(f'sequence_length : {sequence_length}')
+
+    analyzer = Analyzer()
+
+    while True:
+        num = random.randint(1,100)
+        analyzer.add_number(num)
+
+        if len(analyzer.numbers) > sequence_length:
+            analyzer.numbers.pop(0)
+
+        print(f"total even: {analyzer.even_count()}") 
+        print(f'total odd: {analyzer.odd_count()}')
+        print(f"highest number: {analyzer.highest_number()}") 
+        print(f"increasing pairs: {analyzer.increasing_pairs()}")
+
+        t = datetime.now()
+
+        if len(analyzer.numbers) >= sequence_length and t.second == 0:
+            break
+
+        time.sleep(interval)
+
+if __name__ == "__main__":
+    main()
